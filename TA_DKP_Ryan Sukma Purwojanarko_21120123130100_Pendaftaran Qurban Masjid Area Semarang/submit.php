4c737b809860d634fc552de5bd4e7af0c7730baa
RYAN SUKMA PURWOJANARKO <ryansukma1208@gmail.com>
2024-05-28 06:35:41 +0700

    submit.php

--- TA_DKP_Ryan Sukma Purwojanarko_21120123130100_Pendaftaran Qurban Masjid Area Semarang/submit.php
+++ TA_DKP_Ryan Sukma Purwojanarko_21120123130100_Pendaftaran Qurban Masjid Area Semarang/submit.php
@@ -17,7 +17,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <head>
             <meta charset='UTF-8'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>Pendaftaran Berhasil</title>
             <link rel='stylesheet' href='style2.css'>
         </head>
         <body>
